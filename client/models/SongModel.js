b2145413906bdce6455179db7ff241ec102dca2c
kent10ou <kent10ou@gmail.com>
2015-07-15 15:25:51 -0700

    songModel spec passed!

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,23 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
-
+  this.url = url,
+  this.title = title,
+  this.artist = artist,
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function () {
+  	this.trigger('enqueue', this);
+  },
+
+  dequeue: function () {
+  	this.trigger('dequeue', this);
+  },
+
+  ended: function () {
+  	this.trigger('ended', this);
   }
 
 });
